a97ed6bdfe6b7cdd6f27b00df8dfbc574e81dde0
nureddinerkaya <muharremnureddin@gmail.com>
2024-12-15 20:18:04 +0300

    Custom Postgresql image'ı oluşturdum. Normalinden tek farkı şifreyi otomatik "postgres" yapıyor olması. User ve db de "postgres".

--- library-management-system-backend/config.py
+++
@@ -1,3 +0,0 @@
-import os
-
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:123456@localhost/postgres")
--- library-management-system-backend/db.py
+++ library-management-system-backend/db.py
@@ -1,12 +1,13 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine
 
-from config import DATABASE_URL
+asyncio_engine = create_async_engine(
+    "postgresql+psycopg://postgres:postgres@0.0.0.0:5432/postgres"
+)
 
-engine = create_async_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+"""SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
 
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+"""
