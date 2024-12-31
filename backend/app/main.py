2c8318852ce669071ebd76d6eb68b5a5a68594c7
nureddinerkaya <muharremnureddin@gmail.com>
2024-12-31 21:22:11 +0300

    BooKBlueprint.py diye bişey oluşturdum. main.py'ı buna bağladım.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,32 +1,34 @@
-from sanic import Sanic
-from sanic.response import json
+from sanic import Sanic, text
 from backend.database import Base, engine
-from book.BookService import *
+from book.BookEntity import BookEntity
+import book.BookBlueprint
 
 app = Sanic("LibraryManagementApp")
 
-## Create database tables
-#@app.listener("before_server_start")
-#async def setup_db(app, loop):
-#    Base.metadata.create_all(bind=engine)
+app.blueprint(book.BookBlueprint.bp)
 
-@app.route("/")
-async def index(request):
+# Create database tables
+@app.listener("before_server_start")
+async def setup_db(app, loop):
+    Base.metadata.create_all(bind=engine)
+    print("Database tables created successfully.")
 
-    return json({"message": "Welcome to the Library Management System"})
+@app.get("/")
+async def hello(request):
+    print ("hello")
+    return text("Hello, World!")
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
 
-new_book = create_book(
-    name="1984",
-    authors="George Orwell",
-    category="Dystopian",
-    date="1949-06-08",
-    ISBN="123-4567890123",
-    pages=328
-)
-print(f"Created book: {new_book.name}, Authors: {new_book.authors}")
 
-print("adadaasdadsad")
+
+
+
+
+
+
+
+
+
